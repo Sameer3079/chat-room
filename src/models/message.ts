fb2fd4fc764fc301435f3a090e0ba94cd123d100
Sameer Basil <smrbasil4@gmail.com>
2023-05-02 19:52:39 +0530

    Used Zod native discriminating union method

--- src/models/message.ts
+++ src/models/message.ts
@@ -1,24 +1,19 @@
 import { z } from 'zod';
 
-export const TextOnlyMessage = z.object({
-  id: z.string(),
-  hasImage: z.literal(false),
-  content: z.string(),
-  createdAt: z.date(),
-});
-export type TextOnlyMessage = z.infer<typeof TextOnlyMessage>;
-
-export const TextWithImageMessage = z.object({
-  id: z.string(),
-  hasImage: z.literal(true),
-  content: z.string(),
-  imageFileName: z.string(),
-  imageUrl: z.string().url(),
-  createdAt: z.date(),
-});
-export type TextWithImageMessage = z.infer<typeof TextWithImageMessage>;
-
-/**
- * Discriminated Union
- */
-export type Message = TextOnlyMessage | TextWithImageMessage;
+export const Message = z.discriminatedUnion('hasImage', [
+  z.object({
+    id: z.string(),
+    hasImage: z.literal(false),
+    content: z.string(),
+    createdAt: z.date(),
+  }),
+  z.object({
+    id: z.string(),
+    hasImage: z.literal(true),
+    content: z.string(),
+    imageFileName: z.string(),
+    imageUrl: z.string().url(),
+    createdAt: z.date(),
+  }),
+]);
+export type Message = z.infer<typeof Message>;
