cab813353a3063054e2baac1e038ac3ff110ba47
Sameer Basil <smrbasil4@gmail.com>
2023-05-05 17:43:27 +0530

    Implemented Playwright Test

--- playwright/smoke.test.ts
+++ playwright/smoke.test.ts
@@ -2,53 +2,53 @@ import { test, expect } from '@playwright/test';
 
 test.setTimeout(35e3);
 
-test('go to /', async ({ page }) => {
-  await page.goto('/');
+// test('go to /', async ({ page }) => {
+//   await page.goto('/');
 
-  await page.waitForSelector(`text=Starter`);
-});
+//   await page.waitForSelector(`text=Starter`);
+// });
 
-test('test 404', async ({ page }) => {
-  const res = await page.goto('/post/not-found');
-  expect(res?.status()).toBe(404);
-});
-
-test('add a post', async ({ page, browser }) => {
-  const nonce = `${Math.random()}`;
+test('send a message', async ({ page, browser }) => {
+  const message = `Playwright-Jest test message - ${Math.random()}`;
 
   await page.goto('/');
-  await page.fill(`[name=title]`, nonce);
-  await page.fill(`[name=text]`, nonce);
-  await page.click(`form [type=submit]`);
+  page.locator(`#messageText`);
+  await page.fill(`input[type=text]`, message);
+  await page
+    .locator(`button[type=button]>div>span`, { hasText: 'Send' })
+    .click();
+  await page.waitForTimeout(15 * 1000);
   await page.waitForLoadState('networkidle');
   await page.reload();
 
-  expect(await page.content()).toContain(nonce);
+  // Testing optimistic update
+  expect(await page.content()).toContain(message);
 
+  // Testing saving to database
   const ssrContext = await browser.newContext({
     javaScriptEnabled: false,
   });
   const ssrPage = await ssrContext.newPage();
   await ssrPage.goto('/');
 
-  expect(await ssrPage.content()).toContain(nonce);
+  expect(await ssrPage.content()).toContain(message);
 });
 
-test('server-side rendering test', async ({ page, browser }) => {
-  // add a post
-  const nonce = `${Math.random()}`;
+// test('server-side rendering test', async ({ page, browser }) => {
+//   // add a post
+//   const nonce = `${Math.random()}`;
 
-  await page.goto('/');
-  await page.fill(`[name=title]`, nonce);
-  await page.fill(`[name=text]`, nonce);
-  await page.click(`form [type=submit]`);
-  await page.waitForLoadState('networkidle');
+//   await page.goto('/');
+//   await page.fill(`[name=title]`, nonce);
+//   await page.fill(`[name=text]`, nonce);
+//   await page.click(`form [type=submit]`);
+//   await page.waitForLoadState('networkidle');
 
-  // load the page without js
-  const ssrContext = await browser.newContext({
-    javaScriptEnabled: false,
-  });
-  const ssrPage = await ssrContext.newPage();
-  await ssrPage.goto('/');
-  expect(await ssrPage.content()).toContain(nonce);
-});
+//   // load the page without js
+//   const ssrContext = await browser.newContext({
+//     javaScriptEnabled: false,
+//   });
+//   const ssrPage = await ssrContext.newPage();
+//   await ssrPage.goto('/');
+//   expect(await ssrPage.content()).toContain(nonce);
+// });
